330ed6005d9613b8d176fb96cc6c235dd833a5ac
liugtk <gtk.carter.liu@gmail.com>
2017-08-27 10:43:29 +0800

    tankcontroller

--- BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -9,3 +9,16 @@
 ATank* ATankPlayerController::GetControlledTank() const {
 	return Cast <ATank>(GetPawn());
 }
+
+void ATankPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	auto PossesedTank = GetControlledTank();
+	if (!PossesedTank) {
+		UE_LOG(LogTemp, Warning, TEXT("PlayController not possesing a tank"));
+	}
+	else {
+		UE_LOG(LogTemp, Warning, TEXT("PlayerController possesing %s!!"), *PossesedTank->GetName() );
+	}
+	UE_LOG(LogTemp, Warning, TEXT("PlayerController Begin Play!!"));
+}
