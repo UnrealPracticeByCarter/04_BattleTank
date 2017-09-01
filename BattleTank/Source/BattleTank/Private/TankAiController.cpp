edc81b279121ec8ad25b748d071c6460abcc86ca
DESKTOP-O16UD8S\Carter Liu <gtk.carter.liu@gmail.com>
2017-09-01 10:45:52 +0800

    addded the AI controller files need to add features on it

---
+++ BattleTank/Source/BattleTank/Private/TankAiController.cpp
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TankAiController.h"
+#include "GameFramework/Actor.h"
+#include "Engine/World.h"
+
+
+
+
+
+ATank* ATankAiController::GetControlledTank() const {
+	return Cast <ATank>(GetPawn());
+}
+
+ATank * ATankAiController::GetPlayerTank() const
+{
+	auto playerPawn = GetWorld()->GetFirstPlayerController()->GetPawn();
+
+	if (!playerPawn) { return nullptr; }
+	return Cast<ATank>(playerPawn);
+	
+	
+}
+
+void ATankAiController::BeginPlay()
+{
+	Super::BeginPlay();
+	auto PlayerTank = GetPlayerTank();
+	if (!PlayerTank) {
+		UE_LOG(LogTemp, Warning, TEXT("AIController Can not find a player tank"));
+	}
+	else {
+		UE_LOG(LogTemp, Warning, TEXT("AIController find the Player Tnak: %s!!"), *PlayerTank->GetName());
+	}
+	UE_LOG(LogTemp, Warning, TEXT("AIController Begin Play!!"));
+}
